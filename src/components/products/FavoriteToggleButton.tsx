aa1f6f7b0fadc087d6ea68f1f505b7fa71fddf6a
Ankit9835 <ankydchrismatic@gmail.com>
2025-04-08 21:08:01 +0530

    fixing deployment issue

--- src/components/products/FavoriteToggleButton.tsx
+++ src/components/products/FavoriteToggleButton.tsx
@@ -3,14 +3,9 @@ import { Button } from '../ui/button'
 import { FaHeart } from 'react-icons/fa'
 
 const FavoriteToggleButton = ({productId}:{productId:string}) => {
-  const [value,setValue] = useState(false)
-  const id = productId
-  if(id){
-    setValue(true)
-  }
   return (
     <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
-
+      {productId}
       <FaHeart />
     </Button>
   )
